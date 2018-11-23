36a263fa7f90e7bcde5c914f2054a1248e5cedbe
maukii <31925951+maukii@users.noreply.github.com>
2018-11-23 20:52:26 +0200

    1st controller

---
+++ xMaxJamit/Assets/_Scripts/MGL scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] int playerNumber = 1;
+    [SerializeField] float movementSpeed = 5f;
+    [SerializeField] float rotationSpeed = 200f;
+
+    public float hor, ver;
+
+    private void FixedUpdate()
+    {
+
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        Debug.Log("Horizontal" + playerNumber.ToString() + " " + "Vertical" + playerNumber.ToString());
+
+        hor *= Time.deltaTime * rotationSpeed;
+        ver *= Time.deltaTime * movementSpeed;
+
+        transform.Translate(0, 0, ver);
+        transform.Rotate(0, hor, 0);
+
+    }
+}
