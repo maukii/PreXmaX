56ee51955de2af5c24b5f570874cfa3bdb566abb
lauaue <Lauri.auerniitty@gmail.com>
2018-11-23 21:42:40 +0200

    scripteiii

---
+++ xMaxJamit/Assets/Scripts/VitaminDestroy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VitaminDestroy : MonoBehaviour {
+
+    public float timeToDie;
+
+    void Update ()
+    {
+        timeToDie -= 1 * Time.deltaTime;
+
+        if (timeToDie <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
