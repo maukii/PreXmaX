3baaffca2dc95345fa5fd29e599fc8c2e5ca4caa
maukii <31925951+maukii@users.noreply.github.com>
2018-11-25 02:24:42 +0200

    changes to main menu

--- xMaxJamit/Assets/_Scripts/MGL scripts/AudioRelative/AudioManager.cs
+++ xMaxJamit/Assets/_Scripts/MGL scripts/AudioRelative/AudioManager.cs
@@ -22,6 +22,8 @@ public class AudioManager : MonoBehaviour
             return;
         }
 
+        DontDestroyOnLoad(gameObject);
+
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
